8612888aeb0dcd8c9483ce52b5a383d2781c1dba
Thatcher Reidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-04-26 14:45:56 +0000

    q4

--- Q4Picture.py
+++ Q4Picture.py
@@ -1,2 +1,20 @@
-# Using the pygame library, draw a simple picture. 
-# It can be anything you like, but you must use at least 2 different types of shapes and 3 different colors.
+import pygame, sys
+
+pygame.init()
+
+Display = pygame.display.set_mode((500,500))
+
+Clock = pygame.time.Clock()
+FPS = 60
+
+while True:
+    Display.fill((255,255,255))
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    # Draw stuff
+
+    pygame.display.flip()
+    Clock.tick(FPS)
