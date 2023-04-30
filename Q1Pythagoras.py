10ebd1553c1d2ae32f385c659927e39637095f86
Thatcher Reidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-04-30 23:55:12 +0000

    work

--- Q1Pythagoras.py
+++ Q1Pythagoras.py
@@ -2,8 +2,8 @@
 # Remember to use prompts for the input and labels for the output. Use the formula a2 + b2 = c2 to calculate your answer.
 import math
 
-a = float(input("Enter the length of a side of the right angle trigle,a: "))
-b = float(input("Enter the length of a side of the right angle trigle, b: "))
+a = float(input("Enter the length of a side of the right angle triangle, a: "))
+b = float(input("Enter the length of a side of the right angle triangle, b: "))
 
 c = math.sqrt(a**2 + b**2)
 
