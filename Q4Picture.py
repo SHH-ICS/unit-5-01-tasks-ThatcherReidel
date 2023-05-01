b4253a892fad15368049dcd2693ef789f6e78af2
Thatcher Reidel <124378590+ThatcherReidel@users.noreply.github.com>
2023-05-01 14:01:17 +0000

    adding code

--- Q4Picture.py
+++ Q4Picture.py
@@ -1,20 +1,35 @@
-import pygame, sys
+import pygame
 
+# Initialize pygame
 pygame.init()
 
-Display = pygame.display.set_mode((500,500))
+# Set up the window
+window_size = (400, 400)
+window = pygame.display.set_mode(window_size)
 
-Clock = pygame.time.Clock()
-FPS = 60
+# Set up the colors
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
 
-while True:
-    Display.fill((255,255,255))
+# Draw a rectangle
+pygame.draw.rect(window, red, pygame.Rect(50, 50, 100, 100))
+
+# Draw a circle
+pygame.draw.circle(window, green, (250, 250), 50)
+
+# Draw a line
+pygame.draw.line(window, blue, (0, 0), (400, 400), 5)
+
+# Update the screen
+pygame.display.update()
+
+# Run the game loop
+running = True
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    # Draw stuff
+            running = False
 
-    pygame.display.flip()
-    Clock.tick(FPS)
+# Quit pygame
+pygame.quit()
